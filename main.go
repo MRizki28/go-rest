75e3f74a8b9f7ba3f18796b1f0803c11b5fdeac2
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-11-17 00:17:41 +0800

    setup cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/MRizki28/go-rest/config/cors"
 	"github.com/MRizki28/go-rest/controllers/MahasiswaController"
 	"github.com/MRizki28/go-rest/models"
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	r :=	gin.Default();
+	r.Use(cors.CorsMiddleware())
 	models.ConnectionDatabase()
 	models.Migrate()
 
